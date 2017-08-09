2c101a94f5794b683e0e865296584566fe62f0af
WaqasaliOS <waqas.ali@wewanttraffic.pk>
2017-08-09 15:44:18 -0700

    first commit

---
+++ BaseProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BaseProject
+//
+//  Created by Waqas Ali on 09/08/2017.
+//  Copyright © 2017 Waqas Ali. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
